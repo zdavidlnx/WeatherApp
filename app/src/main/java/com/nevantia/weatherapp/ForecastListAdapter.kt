4cf1374b929ad702982b64adecc8b601a7c9f8bb
David García Alonso <g.a.david@gmail.com>
2016-03-23 15:06:05 +0100

    Final CH 10

--- app/src/main/java/com/nevantia/weatherapp/ForecastListAdapter.kt
+++ app/src/main/java/com/nevantia/weatherapp/ForecastListAdapter.kt
@@ -3,20 +3,23 @@ package com.nevantia.weatherapp
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import android.widget.TextView
+import com.nevantia.weatherapp.domain.model.ForecastList
 
 /**
  * Created by d_garcia on 21/03/2016.
  */
 
-class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder> () {
+class ForecastListAdapter(val weekForecast: ForecastList) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder> () {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(TextView(parent.context))
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-       holder.textView.text = items[position]
+       with(weekForecast.dailyForecast[position]) {
+           holder.textView.text = "$date - $description - $high/$low"
+       }
     }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int = weekForecast.dailyForecast.size
 
 
 
--- app/src/main/java/com/nevantia/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/nevantia/weatherapp/ui/MainActivity.kt
@@ -6,29 +6,29 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.nevantia.weatherapp.ForecastListAdapter
 import com.nevantia.weatherapp.R
+import com.nevantia.weatherapp.domain.commands.RequestForecastCommand
+import org.jetbrains.anko.async
+import org.jetbrains.anko.find
+import org.jetbrains.anko.uiThread
 
 
 class MainActivity : AppCompatActivity() {
-
-    private val items = listOf(
-            "Mon 6/23 - Sunny - 31/17",
-            "Tue 6/24 - Foggy - 21/8",
-            "Wed 6/25 - Cloudy - 22/17",
-            "Thurs 6/26 - Rainy - 18/11",
-            "Fri 6/27 - Foggy - 21/10",
-            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-            "Sun 6/29 - Sunny - 20/7"
-    )
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        val forecastList: RecyclerView  = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
-        forecastList.adapter = ForecastListAdapter(items)
+
+        async() {
+            val result = RequestForecastCommand("94043").execute()
+
+            uiThread {
+                forecastList.adapter = ForecastListAdapter(result)
+            }
+        }
 
     }
 }
