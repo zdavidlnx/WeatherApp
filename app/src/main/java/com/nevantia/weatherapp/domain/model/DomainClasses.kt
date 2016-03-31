d755bc962c116f3b2a66f9f115b55c92e729a4d7
David García Alonso <g.a.david@gmail.com>
2016-03-31 22:54:50 +0200

    CH 12 - Inicio

--- app/src/main/java/com/nevantia/weatherapp/domain/model/DomainClasses.kt
+++ app/src/main/java/com/nevantia/weatherapp/domain/model/DomainClasses.kt
@@ -5,7 +5,12 @@ package com.nevantia.weatherapp.domain.model
  * Created by d_garcia on 23/03/2016.
  */
 
-data class ForecastList(val city: String, val country: String,
-                        val dailyForecast: List<Forecast>)
-data class Forecast(val date: String, val description: String, val high: Int,
-                    val low: Int)
+data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>){
+
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+
+    fun size(): Int = dailyForecast.size
+}
+
+
+data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
